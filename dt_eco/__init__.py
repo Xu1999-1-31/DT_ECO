7e969e8fa62fc9e01119ce8b69e9a393e07bf1ce
JiajieXu <1079253792@qq.com>
2024-10-15 11:31:58 +0800

    Remove files not needed

--- dt_eco/__init__.py
+++
@@ -1,8 +0,0 @@
-from gymnasium.envs.registration import register
-from mo_gymnasium.envs.dt_eco import dt_eco
-
-register(
-    id="dt-eco-v0",
-    entry_point="mo_gymnasium.envs.dt_eco.dt_eco:DT_ECO",
-    kwargs={'current_design': 'aes_cipher_top'}
-)
--- work/list_pin_bbox.tcl
+++
@@ -1,6 +0,0 @@
-set pins [get_pins -hier -filter "is_hierarchical==false && full_name!~*/VDD && full_name!~*/VSS && full_name!~*/VNW && full_name!~*/VPW && full_name!~*FILL* && full_name!~*ENDCAP* && full_name!~*DCAP*"]
-foreach_in_collection pin $pins {
-	set pin_name [get_attribute $pin full_name]
-	set bbox [get_attribute $pin bbox]
-	puts "$pin_name $bbox"
-}
